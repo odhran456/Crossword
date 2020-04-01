665cd8ed3869e8d1db8e8345f01ff2f1d683d553
Odhran McHugh <44904580+odhran456@users.noreply.github.com>
2020-04-01 13:01:59 +0100

    can find words now

--- crossword_gui.py
+++ crossword_gui.py
@@ -1,6 +1,7 @@
 import pygame
 import pandas as pd
 
+
 # Pick which crossword template you want to play with
 boundaries = {'version_1':[(0,1),(0,3),(0,5),(0,6),(1,8),(1,10),(1,12),(2,1),(2,3),(2,5),(2,6),(3,5),(3,10),(3,12),
                   (4,4),(4,9),(5,0),(5,2),(5,7),(5,12),(6,0),(6,1),(6,11),(6,12),(7,0),(7,5),(7,10),(7,12),
@@ -15,45 +16,101 @@ boundaries = {'version_1':[(0,1),(0,3),(0,5),(0,6),(1,8),(1,10),(1,12),(2,1),(2,
 tiles = [(i,j) for i in range(13) for j in range(13)]
 
 
-# Get co-ordinate of start of every word
+# Get co-ordinate of start of every word in a list
 def get_word_coords(tiles,version):
     word_coords = []
     for count in range(169):
+        word = {}
         if tiles[count] in boundaries[version]:
             continue
         elif tiles[count] == (0,0):
-            word_coords.append(tiles[count])
+            if tiles[count + 1] not in boundaries[version]:
+                word.update({'across':tiles[count]})
+            if tiles[count + 13] not in boundaries[version]:
+                word.update({'down':tiles[count]})
         elif tiles[count][0] == 0:
-            if tiles[count - 1] in boundaries[version] or tiles[count + 13] not in boundaries[version]:
-                word_coords.append(tiles[count])
+            if tiles[count - 1] in boundaries[version] and tiles[count + 1] not in boundaries[version]:
+                word.update({'across':tiles[count]})
+            elif tiles[count + 13] not in boundaries[version]:
+                word.update({'down':tiles[count]})
         elif tiles[count][1] == 0:
-            if tiles[count - 13] in boundaries[version] or tiles[count + 1] not in boundaries[version]:
-                word_coords.append(tiles[count])
+            if count < 168:
+                if tiles[count + 1] not in boundaries[version]:
+                    word.update({'across':tiles[count]})
+            if count < 155:
+                if tiles[count - 13] in boundaries[version] and tiles[count + 13] not in boundaries[version]:
+                    word.update({'down':tiles[count]})
         if count < 168:
             if tiles[count - 1] in boundaries[version] and tiles[count + 1] not in boundaries[version]:
-                word_coords.append(tiles[count])
+                word.update({'across':tiles[count]})
         if count < 155:
             if tiles[count - 13] in boundaries[version] and tiles[count + 13] not in boundaries[version]:
-                word_coords.append(tiles[count])
+                word.update({'down':tiles[count]})
         count+=1
-    word_coords = list( dict.fromkeys(word_coords))
-    return word_coords
+        word_coords.append(word)
+    word_coords = list(filter(None, word_coords))
+    words_df = pd.DataFrame(word_coords)
+    return words_df
 
 
-# TODO :Get co-ordinates of all letters of word for all words
-def words_full(word_coords):
-    all_words = []
-    for i in range(len(word_coords)):
-        index = tiles.index(word_coords[i])
-        cur = tiles[index]
-        word_len = []
-        while index % 13 <= 12 and cur not in boundaries['version_1']:
-            word_len.append(cur)
-            index += 1
-            cur = tiles[index]
-        if len(word_len) > 1:
-            all_words.append(word_len)
-    return all_words
+def find_end_across(start):
+    same_row = []
+    for i in range(len(boundaries['version_1'])):
+        if boundaries['version_1'][i][0] == start[0] and boundaries['version_1'][i][1] > start[1]:
+            same_row.append(boundaries['version_1'][i])
+    if not same_row:
+        end_coord = (start[0],12)
+    else:
+        end_coord = (same_row[0][0],same_row[0][1]-1)
+    print(start,end_coord)
+    return end_coord
+
+
+def find_end_down(start):
+    same_column = []
+    for i in range(len(boundaries['version_1'])):
+        if boundaries['version_1'][i][1] == start[1] and boundaries['version_1'][i][0] > start[0]:
+            same_column.append(boundaries['version_1'][i])
+    if not same_column:
+        end_coord = (12,start[1])
+    else:
+        end_coord = (same_column[0][0]-1,same_column[0][1])
+    print(start,end_coord)
+    return end_coord
+
+
+def find_coords_across(start,end):
+    word = []
+    for i in range(start[1],end[1]+1,1):
+        word.append((start[0],i))
+    return word
+
+
+def find_coords_down(start,end):
+    word = []
+    for i in range(start[0],end[0]+1,1):
+        word.append((i,start[1]))
+    return word
+
+
+def words_full(df):
+    list_of_words = []
+    for i in range(df.shape[0]):
+        if type(df.iloc[i]['across']) is tuple:
+            end = find_end_across(df.iloc[i]['across'])
+            coords_of_word = find_coords_across(df.iloc[i]['across'],end)
+            list_of_words.append([df.iloc[i]['across'],'across',len(coords_of_word),coords_of_word])
+        if type(df.iloc[i]['down']) is tuple:
+            end = find_end_down(df.iloc[i]['down'])
+            coords_of_word = find_coords_down(df.iloc[i]['down'],end)
+            list_of_words.append([df.iloc[i]['down'],'down',len(coords_of_word),coords_of_word])
+    words_full_df = pd.DataFrame(list_of_words,columns=['start','drn','len','coords'])
+    print(words_full_df)
+    return words_full_df
+
+
+a = get_word_coords(tiles,'version_1')
+words_full(a)
 
 
 class Grid:
@@ -103,7 +160,6 @@ def main():
     key = None
     run = True
     while run:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -142,3 +198,4 @@ def main():
 
 
 main()
+
